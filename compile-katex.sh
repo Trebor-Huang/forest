6f148edb6abf3d09b843090c3cb4fc38ad67db13
Trebor-Huang <treborhuang@foxmail.com>
2024-03-01 12:48:27 +0800

    Peripheral

---
+++ compile-katex.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+node theme/javascript-source/convert.js
--- watch.sh
+++ watch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 rm -rf build/
+rm -rf output/
 forester build --dev --root=trebor-0001 trees/
 
-fswatch -o trees/ | while read num ; \
+fswatch -o trees/ theme/ | while read num ; \
   do \
     clear
     echo "Rebuilding forest"
