f25a5d9ba339152dcb939974e779a6b5aaaf6601
Trebor-Huang <treborhuang@foxmail.com>
2024-04-07 22:46:29 +0800

    Write

--- watch.sh
+++ watch.sh
@@ -3,11 +3,12 @@
 rm -rf build/
 rm -rf output/
 forester build forest.toml --dev
+echo "Cleanup build finished, watching..."
 
 fswatch -o trees/ theme/ | while read num ; \
   do \
     clear
-    echo "Rebuilding forest"
+    echo "Rebuilding forest..."
     time forester build forest.toml --dev
     echo "Done"
   done
